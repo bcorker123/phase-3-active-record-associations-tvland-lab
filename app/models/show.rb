ebd84980b8cba7de842d52d5ce48ce601938c2ca
brian corker <bcorker123@gmail.com>
2022-06-13 11:19:22 -0400

    commit tree a61ac220402e2463eb44a5db44221cfd15723ff9

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,13 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  belongs_to :network
+  has_many :actors, through: :characters
+
+  def actors_list
+    actors_list = []
+    self.actors.each do |actor|
+        actors_list << actor.full_name
+    end
+    actors_list
+  end   
 end
